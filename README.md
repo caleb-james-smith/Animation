3187d513e1983e3590c3b9b02f2b110c0b893deb
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-09-06 18:43:04 -0500

    Rename input/output and change gif duration.

--- README.md
+++ README.md
@@ -7,11 +7,12 @@ Download repository:
 git clone git@github.com:caleb-james-smith/Animation.git
 cd Animation
 ```
-Create an album (e.g. Google photos album), download, copy, and unzip.
+Create an album (e.g. Google photos album), download, move, unzip, and rename.
 ```
 cd images
 mv ~/Downloads/The_KU_Tree-001.zip .
 unzip The_KU_Tree-001.zip
+mv The_KU_Tree The_KU_Tree_2022_09_06_v1
 cd ..
 ```
 Run python script to create gif.
--- python/make_gif.py
+++ python/make_gif.py
@@ -10,7 +10,7 @@ def makeDir(dir_name):
         os.makedirs(dir_name)
 
 def make_gif(image_dir, output_dir, output_file):
-    output_name = "{0}/{1}".format(output_dir, output_file)
+    output_path = "{0}/{1}".format(output_dir, output_file)
     images      = glob.glob(image_dir + "/*.jpg")
     
     makeDir(output_dir)
@@ -19,15 +19,21 @@ def make_gif(image_dir, output_dir, output_file):
     
     print("Making a gif using images from \"{0}\".".format(image_dir))
     print("Number of images: {0}".format(n_images))
+
+    for image in images:
+        print(image)
     
+    duration    = 500
     frames      = [Image.open(image) for image in images]
     first_frame = frames[0]
-    first_frame.save(output_name, format="GIF", append_images=frames, save_all=True, duration=100, loop=0)
+    first_frame.save(output_path, format="GIF", append_images=frames, save_all=True, duration=duration, loop=0)
+    
+    print("Output file: {0}".format(output_path))
 
 def main():
-    image_dir   = "images/The_KU_Tree"
+    image_dir   = "images/The_KU_Tree_2022_09_06_v1"
     output_dir  = "gifs"
-    output_file = "The_KU_Tree.gif"
+    output_file = "The_KU_Tree_2022_09_06_v1.gif"
     make_gif(image_dir, output_dir, output_file)
 
 if __name__ == '__main__':
