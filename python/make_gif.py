75baca4e2046a138dade5256ac425543e664a9a5
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-09-07 18:01:20 -0500

    Create combined images and gif.

--- python/make_gif.py
+++ python/make_gif.py
@@ -11,21 +11,21 @@ def makeDir(dir_name):
         os.makedirs(dir_name)
 
 # combine image file into a single image
-def combineImages(files):
+def combineImages(files, output_file):
     images = [Image.open(f) for f in files]
     widths, heights = zip(*(i.size for i in images))
     total_width = sum(widths)
     max_height  = max(heights)
     
-    print("total_width  = {0}".format(total_width))
-    print("max_height   = {0}".format(max_height))
+    #print("total_width  = {0}".format(total_width))
+    #print("max_height   = {0}".format(max_height))
     
     new_image = Image.new('RGB', (total_width, max_height))
     x_offset = 0
     for image in images:
         new_image.paste(image, (x_offset, 0))
         x_offset += image.size[0]
-    new_image.save("combined_example_1.jpg")
+    new_image.save(output_file)
 
 # create gif of all images in a directory
 def make_gif(image_dir, output_dir, output_file):
@@ -49,43 +49,106 @@ def make_gif(image_dir, output_dir, output_file):
     
     print("Output file: {0}".format(output_path))
 
-def run_standard():
-    start_time = time.time()
+# create combined images
+# assume same image names in both input directories
+def create_combined(image_dir_1, image_dir_2, output_dir):
+    print("Creating combined images.")
+    images_1 = glob.glob(image_dir_1 + "/*.jpg")
+    images_2 = glob.glob(image_dir_2 + "/*.jpg")
     
-    # all images of tree
-    image_dir   = "images/The_KU_Tree_2022_09_06_v1"
+    makeDir(output_dir)
+    images_1.sort()
+    images_2.sort()
+    n_images_1 = len(images_1)
+    n_images_2 = len(images_2)
+    
+    # check that the same number of images are found
+    if n_images_1 != n_images_2:
+        print("ERROR: Different number of images found!")
+        print(" - image_dir_1: {0}".format(image_dir_1))
+        print(" - image_dir_2: {0}".format(image_dir_2))
+        print(" - n_images_1:  {0}".format(n_images_1))
+        print(" - n_images_2:  {0}".format(n_images_2))
+        return
+    
+    #for image in images_1:
+    #    print(image)
+    #for image in images_2:
+    #    print(image)
+    
+    for i1, i2 in zip(images_1, images_2):
+        files   = [i1, i2]
+        name_1  = os.path.basename(i1)
+        name_2  = os.path.basename(i2)
+        
+        # check that file names match
+        if name_1 != name_2:
+            print("ERROR: File names do not match!")
+            print(" - name_1: {0}".format(name_1))
+            print(" - name_2: {0}".format(name_2))
+            return
+
+        output_file = "{0}/{1}".format(output_dir, name_1)
+        combineImages(files, output_file)
+        
+        #print(i1, i2)
+        print(name_1)
+    
+    print("Output directory: {0}".format(output_dir))
+
+def test_combine():
+    files = [
+        "images/The_KU_Tree_2022_09_06_v1_split/west_side/PXL_20220809_001.jpg",
+        "images/The_KU_Tree_2022_09_06_v1_split/east_side/PXL_20220809_001.jpg",
+    ]
+    output_file = "test_combine_1.jpg"
+    combineImages(files, output_file)
+
+def run_combine():
+    # order images from left to right
+    image_dir_1 = "images/The_KU_Tree_2022_09_06_v1/west_side"
+    image_dir_2 = "images/The_KU_Tree_2022_09_06_v1/east_side"
+    output_dir  = "images/The_KU_Tree_2022_09_06_v1/combined"
+    create_combined(image_dir_1, image_dir_2, output_dir)
+
+# create gifs of files in directory
+def run_standard():
+    # all original images of tree
+    image_dir   = "images/The_KU_Tree_2022_09_06_v1/original"
     output_dir  = "gifs"
-    output_file = "The_KU_Tree_2022_09_06_v1.gif"
+    output_file = "The_KU_Tree_2022_09_06_v1_original.gif"
     make_gif(image_dir, output_dir, output_file)
     
     # west side of tree 
-    image_dir   = "images/The_KU_Tree_2022_09_06_v1_split/west_side"
+    image_dir   = "images/The_KU_Tree_2022_09_06_v1/west_side"
     output_dir  = "gifs"
     output_file = "The_KU_Tree_2022_09_06_v1_west_side.gif"
     make_gif(image_dir, output_dir, output_file)
     
     # east side of tree 
-    image_dir   = "images/The_KU_Tree_2022_09_06_v1_split/east_side"
+    image_dir   = "images/The_KU_Tree_2022_09_06_v1/east_side"
     output_dir  = "gifs"
     output_file = "The_KU_Tree_2022_09_06_v1_east_side.gif"
     make_gif(image_dir, output_dir, output_file)
+    
+    # combined west side and east side images of tree
+    image_dir   = "images/The_KU_Tree_2022_09_06_v1/combined"
+    output_dir  = "gifs"
+    output_file = "The_KU_Tree_2022_09_06_v1_combined.gif"
+    make_gif(image_dir, output_dir, output_file)
+
+def main():
+    start_time = time.time()
+    
+    #test_combine()
+    run_combine()
+    run_standard()
     
     end_time = time.time()
     
     run_time = end_time - start_time
     
     print("run time: {0:.3f} seconds".format(run_time))
-
-def run_combined():
-    files = [
-        "images/The_KU_Tree_2022_09_06_v1_split/west_side/PXL_20220809_001.jpg",
-        "images/The_KU_Tree_2022_09_06_v1_split/east_side/PXL_20220809_001.jpg",
-    ]
-    combineImages(files)
-
-def main():
-    #run_standard()
-    run_combined()
 
 if __name__ == '__main__':
     main()
